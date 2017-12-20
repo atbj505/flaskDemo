b0fca42bf7d352dc1fedf2c4e6e06699bf25b859
atbj505 <atbj505@hotmail.com>
2017-12-20 18:52:17 +0800

    add

--- flaskr.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os
-import sqlite3
-
-from flask import (Flask, abort, flash, g, redirect, render_template, request,
-                   session, url_for)
